0ae799884da0ee0d9a118e91711a525d6f3a89cd
Siddharth Singh <siddharths51700@gmail.com>
2020-10-10 19:39:35 +0530

    deployed!

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyASgFLAXoquYgokqYbHAhrGtUQf1DJAOew",
+  authDomain: "netflixclone-9381a.firebaseapp.com",
+  databaseURL: "https://netflixclone-9381a.firebaseio.com",
+  projectId: "netflixclone-9381a",
+  storageBucket: "netflixclone-9381a.appspot.com",
+  messagingSenderId: "756767440502",
+  appId: "1:756767440502:web:a46d7b3a02d0efc1dc6957",
+  measurementId: "G-NFDF9XW332",
+});
